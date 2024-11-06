2fa381529c2e0dad043456f254fb42be04461cf1
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-11-06 15:14:59 +0100

    manage new functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-const emailListGroupEl = document.querySelector(".list-group");
+//function for create an item in the list
+const createListElement = (email) =>
+  (emailListGroupEl.innerHTML += `
+  <li class="list-group-item">${email}</li>
+  `);
 
-for (let i = 0; i < 10; i++) {
+//function for generate a random email
+const generateRandomEmail = () => {
   fetch("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((response) => response.json())
     .then((data) => {
       console.log(data.response);
 
-      emailListGroupEl.innerHTML += `
-        <li class="list-group-item">${data.response}</li>
-    `;
+      createListElement(data.response);
     });
+};
+
+const emailListGroupEl = document.querySelector(".list-group");
+
+for (let i = 0; i < 10; i++) {
+  generateRandomEmail();
 }
